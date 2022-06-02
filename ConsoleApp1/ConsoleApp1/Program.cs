94b23d905ad4294b25747ec488e994408e642bf7
turalismylv <turalhi@code.edu.az>
2022-06-02 22:25:34 +0400

    HomeWork0306

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tapşırıq: bir metoduz olsun,
+            //        parametr olaraq istənilən data tipdən dəyər ala bilməlidi. 
+            //        Parametrinə gələn dəyəri çap etsin
+
+            int a = 5;
+            string b = "Tural";
+            long c = -5;
+            double d = 10.25;
+            Print(a);
+            Print(b);
+            Print(c);
+            Print(d);
+        }
+        public static void Print(Object o) 
+        {
+            Console.WriteLine(o);
+        
+        
+        }
+    }
+}
